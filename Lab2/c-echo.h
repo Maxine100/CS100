60737ffcd7bd15831e8f7ffd60556cfebd36b873
Maxine Wu <maxine.wu00@gmail.com>
2020-08-25 22:23:56 -0700

    Created main.cpp, c-echo.h, CMakeLists.txt, updated .gitignore.

--- Lab2/c-echo.h
+++ Lab2/c-echo.h
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int main(int argv, char** argc) {
+/* int main(int argv, char** argc) {
 	// Skip the first argc index because it's the program.
 	for (int i = 1; i < argv; i++) {
 		std::cout << argc[i];
@@ -12,4 +12,16 @@ int main(int argv, char** argc) {
 	std::cout << std::endl;
 }
 // Compile with "g++ c-echo.cpp -o c-echo".
-// Then run "./c-echo" followed by other inputs, which should be echoed.
+// Then run "./c-echo" followed by other inputs, which should be echoed. */
+
+std::string echo(int length, char** chars) {
+	std::string ret = "";
+	for (int i = 1; i < length; i++) {
+		ret += chars[i];
+		if (i < length - 1) {
+			ret += " ";
+		}
+	}
+	ret += "\n";
+	return ret;
+}
