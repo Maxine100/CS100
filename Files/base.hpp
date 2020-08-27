15c82fe3a4628d5ca7734396505242ff5b8d7701
Maxine Wu <maxine.wu00@gmail.com>
2020-08-26 22:39:26 -0700

    Create base.hpp.

---
+++ Files/base.hpp
@@ -0,0 +1,14 @@
+#ifndef BASE_HPP
+#define BASE_HPP
+
+class Base {
+	public:
+		/* Constructors */
+		Base() { };
+
+		/* Pure Virtual Functions */
+		virtual double evaluate() = 0;
+		virtual string stringify() = 0;
+};
+
+#endif
