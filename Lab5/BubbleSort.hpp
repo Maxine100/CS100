c6cf18e2aff9c4faeb61eb0e18968ad8d963a608
Maxine Wu <maxine.wu00@gmail.com>
2020-08-27 22:54:07 -0700

    Update BubbleSort.hpp.

--- Lab5/BubbleSort.hpp
+++ Lab5/BubbleSort.hpp
@@ -1,17 +1,17 @@
 #ifndef BUBBLESORT_HPP
 #define BUBBLESORT_HPP
 
-class BubbleSort : public Sort() {
+class BubbleSort : public Sort {
 	public:
-		BubbleSort() : public Sort { }
+		BubbleSort() : Sort() { }
 		void sort(Container* container) {
 			int i;
 			int j;
 			int flag;
 			int temp;
-			for (i = 1; (i < container->size()) && flag; i++) {
+			for (i = 1; (i <= container->size()) && flag; i++) {
 				flag = 0;
-				for (j = 0; j < container->size() - 1;j++) {
+				for (j = 0; j < container->size() - 1; j++) {
 					if (container->at(j + 1) > container->at(j)) {
 						container->swap(j, j + 1);
 						flag = 1;
